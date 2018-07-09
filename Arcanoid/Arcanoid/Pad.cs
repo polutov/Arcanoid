7035c796f3a10b2ced76d7c0ebc35f45469162ce
polutov <polutov@gmail.com>
2018-07-09 07:59:50 +0300

    Внес изменения в класс pad

--- Arcanoid/Arcanoid/Pad.cs
+++ Arcanoid/Arcanoid/Pad.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,10 @@ namespace Arcanoid
 	class Pad
 	{
 		public int X { get; set; }
+		public int Y { get; set; }
 		public int Width { get; set; }
+		public int Height { get; set; }
+
+		public Rectangle Rect { get { return new Rectangle(X, Y, Width, Height); } }
 	}
 }
