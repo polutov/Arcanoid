cb2da55183c3d0a8c794186ead182dd2c1474bc9
polutov <polutov@gmail.com>
2018-07-09 10:29:39 +0300

    Изменил форму и добавил графические элементы для Ball, Pad, Brick

--- Arcanoid/Arcanoid/frmMain.Designer.cs
+++ Arcanoid/Arcanoid/frmMain.Designer.cs
@@ -28,121 +28,228 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
-			this.pnlMainMenu = new System.Windows.Forms.Panel();
-			this.btnExit = new System.Windows.Forms.Button();
-			this.btnOptions = new System.Windows.Forms.Button();
-			this.btnNewGame = new System.Windows.Forms.Button();
-			this.lblCredit = new System.Windows.Forms.Label();
+			this.tmrUpdate = new System.Windows.Forms.Timer(this.components);
+			this.ilBalls = new System.Windows.Forms.ImageList(this.components);
+			this.ilBricks = new System.Windows.Forms.ImageList(this.components);
+			this.ilPads = new System.Windows.Forms.ImageList(this.components);
 			this.pbLogo = new System.Windows.Forms.PictureBox();
-			this.pnlMainMenu.SuspendLayout();
+			this.gbSpeed = new System.Windows.Forms.GroupBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.gbLevel = new System.Windows.Forms.GroupBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.gbLives = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.gbScore = new System.Windows.Forms.GroupBox();
+			this.lblMultiplier = new System.Windows.Forms.Label();
+			this.lblScore = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+			this.gbSpeed.SuspendLayout();
+			this.gbLevel.SuspendLayout();
+			this.gbLives.SuspendLayout();
+			this.gbScore.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// pnlMainMenu
+			// tmrUpdate
 			// 
-			this.pnlMainMenu.Controls.Add(this.btnExit);
-			this.pnlMainMenu.Controls.Add(this.btnOptions);
-			this.pnlMainMenu.Controls.Add(this.btnNewGame);
-			this.pnlMainMenu.Controls.Add(this.lblCredit);
-			this.pnlMainMenu.Controls.Add(this.pbLogo);
-			this.pnlMainMenu.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.pnlMainMenu.Location = new System.Drawing.Point(0, 0);
-			this.pnlMainMenu.Name = "pnlMainMenu";
-			this.pnlMainMenu.Size = new System.Drawing.Size(1006, 721);
-			this.pnlMainMenu.TabIndex = 0;
+			this.tmrUpdate.Interval = 1;
 			// 
-			// btnExit
+			// ilBalls
 			// 
-			this.btnExit.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.btnExit.ForeColor = System.Drawing.SystemColors.ButtonFace;
-			this.btnExit.Location = new System.Drawing.Point(403, 428);
-			this.btnExit.Name = "btnExit";
-			this.btnExit.Size = new System.Drawing.Size(200, 32);
-			this.btnExit.TabIndex = 4;
-			this.btnExit.Text = "Exit";
-			this.btnExit.UseVisualStyleBackColor = true;
-			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			this.ilBalls.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilBalls.ImageStream")));
+			this.ilBalls.TransparentColor = System.Drawing.Color.Transparent;
+			this.ilBalls.Images.SetKeyName(0, "ballBlue.png");
+			this.ilBalls.Images.SetKeyName(1, "ballGrey.png");
 			// 
-			// btnOptions
+			// ilBricks
 			// 
-			this.btnOptions.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.btnOptions.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.btnOptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.btnOptions.ForeColor = System.Drawing.SystemColors.ButtonFace;
-			this.btnOptions.Location = new System.Drawing.Point(403, 390);
-			this.btnOptions.Name = "btnOptions";
-			this.btnOptions.Size = new System.Drawing.Size(200, 32);
-			this.btnOptions.TabIndex = 3;
-			this.btnOptions.Text = "Options";
-			this.btnOptions.UseVisualStyleBackColor = true;
+			this.ilBricks.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilBricks.ImageStream")));
+			this.ilBricks.TransparentColor = System.Drawing.Color.Transparent;
+			this.ilBricks.Images.SetKeyName(0, "element_grey_rectangle.png");
+			this.ilBricks.Images.SetKeyName(1, "element_blue_rectangle.png");
+			this.ilBricks.Images.SetKeyName(2, "element_green_rectangle.png");
+			this.ilBricks.Images.SetKeyName(3, "element_yellow_rectangle.png");
+			this.ilBricks.Images.SetKeyName(4, "element_red_rectangle.png");
+			this.ilBricks.Images.SetKeyName(5, "element_purple_rectangle.png");
 			// 
-			// btnNewGame
+			// ilPads
 			// 
-			this.btnNewGame.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.btnNewGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.btnNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.btnNewGame.ForeColor = System.Drawing.SystemColors.ButtonFace;
-			this.btnNewGame.Location = new System.Drawing.Point(403, 352);
-			this.btnNewGame.Name = "btnNewGame";
-			this.btnNewGame.Size = new System.Drawing.Size(200, 32);
-			this.btnNewGame.TabIndex = 2;
-			this.btnNewGame.Text = "New Game";
-			this.btnNewGame.UseVisualStyleBackColor = true;
-			// 
-			// lblCredit
-			// 
-			this.lblCredit.AutoSize = true;
-			this.lblCredit.ForeColor = System.Drawing.Color.White;
-			this.lblCredit.Location = new System.Drawing.Point(871, 695);
-			this.lblCredit.Name = "lblCredit";
-			this.lblCredit.Size = new System.Drawing.Size(123, 17);
-			this.lblCredit.TabIndex = 1;
-			this.lblCredit.Text = "by Dmytro Polutov";
-			this.lblCredit.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+			this.ilPads.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilPads.ImageStream")));
+			this.ilPads.TransparentColor = System.Drawing.Color.Transparent;
+			this.ilPads.Images.SetKeyName(0, "paddleBlu.png");
+			this.ilPads.Images.SetKeyName(1, "paddleRed.png");
 			// 
 			// pbLogo
 			// 
-			this.pbLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
 			this.pbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbLogo.Image")));
-			this.pbLogo.InitialImage = null;
-			this.pbLogo.Location = new System.Drawing.Point(250, 150);
+			this.pbLogo.Location = new System.Drawing.Point(867, 13);
+			this.pbLogo.Margin = new System.Windows.Forms.Padding(4);
 			this.pbLogo.Name = "pbLogo";
-			this.pbLogo.Size = new System.Drawing.Size(500, 156);
-			this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.pbLogo.TabIndex = 0;
+			this.pbLogo.Size = new System.Drawing.Size(267, 76);
+			this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pbLogo.TabIndex = 9;
 			this.pbLogo.TabStop = false;
+			// 
+			// gbSpeed
+			// 
+			this.gbSpeed.BackColor = System.Drawing.Color.White;
+			this.gbSpeed.Controls.Add(this.label3);
+			this.gbSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.gbSpeed.Location = new System.Drawing.Point(867, 365);
+			this.gbSpeed.Margin = new System.Windows.Forms.Padding(4);
+			this.gbSpeed.Name = "gbSpeed";
+			this.gbSpeed.Padding = new System.Windows.Forms.Padding(4);
+			this.gbSpeed.Size = new System.Drawing.Size(267, 74);
+			this.gbSpeed.TabIndex = 8;
+			this.gbSpeed.TabStop = false;
+			this.gbSpeed.Text = "Speed";
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(59, 27);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(200, 39);
+			this.label3.TabIndex = 1;
+			this.label3.Text = "12345689";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// gbLevel
+			// 
+			this.gbLevel.BackColor = System.Drawing.Color.White;
+			this.gbLevel.Controls.Add(this.label2);
+			this.gbLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.gbLevel.Location = new System.Drawing.Point(867, 284);
+			this.gbLevel.Margin = new System.Windows.Forms.Padding(4);
+			this.gbLevel.Name = "gbLevel";
+			this.gbLevel.Padding = new System.Windows.Forms.Padding(4);
+			this.gbLevel.Size = new System.Drawing.Size(267, 74);
+			this.gbLevel.TabIndex = 7;
+			this.gbLevel.TabStop = false;
+			this.gbLevel.Text = "Level";
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(59, 27);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(200, 39);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "12345689";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// gbLives
+			// 
+			this.gbLives.BackColor = System.Drawing.Color.White;
+			this.gbLives.Controls.Add(this.label1);
+			this.gbLives.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.gbLives.Location = new System.Drawing.Point(867, 203);
+			this.gbLives.Margin = new System.Windows.Forms.Padding(4);
+			this.gbLives.Name = "gbLives";
+			this.gbLives.Padding = new System.Windows.Forms.Padding(4);
+			this.gbLives.Size = new System.Drawing.Size(267, 74);
+			this.gbLives.TabIndex = 6;
+			this.gbLives.TabStop = false;
+			this.gbLives.Text = "Lives";
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(59, 27);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(200, 39);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "12345689";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// gbScore
+			// 
+			this.gbScore.BackColor = System.Drawing.Color.White;
+			this.gbScore.Controls.Add(this.lblMultiplier);
+			this.gbScore.Controls.Add(this.lblScore);
+			this.gbScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.gbScore.Location = new System.Drawing.Point(867, 97);
+			this.gbScore.Margin = new System.Windows.Forms.Padding(4);
+			this.gbScore.Name = "gbScore";
+			this.gbScore.Padding = new System.Windows.Forms.Padding(4);
+			this.gbScore.Size = new System.Drawing.Size(267, 98);
+			this.gbScore.TabIndex = 5;
+			this.gbScore.TabStop = false;
+			this.gbScore.Text = "Score";
+			// 
+			// lblMultiplier
+			// 
+			this.lblMultiplier.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.lblMultiplier.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.lblMultiplier.Location = new System.Drawing.Point(59, 65);
+			this.lblMultiplier.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lblMultiplier.Name = "lblMultiplier";
+			this.lblMultiplier.Size = new System.Drawing.Size(200, 30);
+			this.lblMultiplier.TabIndex = 1;
+			this.lblMultiplier.Text = "x1";
+			this.lblMultiplier.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+			// 
+			// lblScore
+			// 
+			this.lblScore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.lblScore.Location = new System.Drawing.Point(59, 27);
+			this.lblScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lblScore.Name = "lblScore";
+			this.lblScore.Size = new System.Drawing.Size(200, 39);
+			this.lblScore.TabIndex = 0;
+			this.lblScore.Text = "12345689";
+			this.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
 			// 
 			// frmMain
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.Black;
-			this.ClientSize = new System.Drawing.Size(1006, 721);
-			this.Controls.Add(this.pnlMainMenu);
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(1147, 888);
+			this.Controls.Add(this.pbLogo);
+			this.Controls.Add(this.gbSpeed);
+			this.Controls.Add(this.gbLevel);
+			this.Controls.Add(this.gbLives);
+			this.Controls.Add(this.gbScore);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
 			this.Name = "frmMain";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Arcanoid";
-			this.pnlMainMenu.ResumeLayout(false);
-			this.pnlMainMenu.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+			this.gbSpeed.ResumeLayout(false);
+			this.gbLevel.ResumeLayout(false);
+			this.gbLives.ResumeLayout(false);
+			this.gbScore.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.Panel pnlMainMenu;
+		private System.Windows.Forms.Timer tmrUpdate;
+		private System.Windows.Forms.ImageList ilBalls;
+		private System.Windows.Forms.ImageList ilBricks;
+		private System.Windows.Forms.ImageList ilPads;
 		private System.Windows.Forms.PictureBox pbLogo;
-		private System.Windows.Forms.Label lblCredit;
-		private System.Windows.Forms.Button btnNewGame;
-		private System.Windows.Forms.Button btnExit;
-		private System.Windows.Forms.Button btnOptions;
+		private System.Windows.Forms.GroupBox gbSpeed;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.GroupBox gbLevel;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.GroupBox gbLives;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox gbScore;
+		private System.Windows.Forms.Label lblMultiplier;
+		private System.Windows.Forms.Label lblScore;
 	}
 }
 
