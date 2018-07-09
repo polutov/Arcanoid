00cae91e1288b9a20e16a15443808384aab8a95e
polutov <polutov@gmail.com>
2018-07-09 08:01:31 +0300

    Внес изменения в класс Brick добавил свойство Rect для расчета коллизий

--- Arcanoid/Arcanoid/Brick.cs
+++ Arcanoid/Arcanoid/Brick.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,9 @@ namespace Arcanoid
 		public int HP { get; set; }
 		public int X { get; set; }
 		public int Y { get; set; }
+		public int Width { get; set; }
+		public int Height { get; set; }
+
+		public Rectangle Rect { get { return new Rectangle(X, Y, Width, Height); } }
 	}
 }
